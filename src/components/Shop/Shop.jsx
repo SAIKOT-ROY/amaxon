87d7fe9ea73c10a73f2e26f3a6784867bd7886ae
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2023-03-28 20:06:04 +0600

    retrive

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import save from "save";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Products/Product";
 import "./Shop.css";
@@ -12,10 +14,28 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  useEffect( () => {
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find(product => product.id === id);
+        if(addedProduct){
+          const quantity = storedCart[id];
+          addedProduct.quantity = quantity;
+          savedCart.push(addedProduct)
+        }
+        setCart(savedCart)
+    }
+  }, [products]);
+
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
+
+
   return (
     <div className="shop-container">
       <div className="products-container">
@@ -28,7 +48,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-       <Cart cart={cart}></Cart>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
