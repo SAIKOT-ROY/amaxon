ee01c0e1f8be8ff799caae39e0b2ea973eddcab4
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2023-03-29 18:17:59 +0600

    last commit from this project

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,20 +3,23 @@ import "./Cart.css";
 
 const Cart = (props) => {
   const { cart } = props;
-  console.log(cart);
 
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
+
   for (const product of cart) {
     totalPrice = totalPrice + product.price;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
+
   const tax = (totalPrice * 7) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h3>Order summary</h3>
-      <p>Selected Items : {cart.length}</p>
+      <p>Selected Items :{quantity}</p>
       <p>Total price :${totalPrice}</p>
       <p>Total shipping :${totalShipping}</p>
       <p>Tax :${tax.toFixed(2)}</p>
